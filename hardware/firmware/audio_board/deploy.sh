9715e96ef24416b6ed00b257ae4042cd8cf010d7
Martijn Braam <martijn@brixit.nl>
2024-10-25 20:35:07 +0200

    firmware: audio_board: improve deployment scripts

--- hardware/firmware/audio_board/deploy.sh
+++ hardware/firmware/audio_board/deploy.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 if [[ $# -ne 1 ]]; then
     echo "usage: ${0} <SSH host of radxa>"
+    exit
 fi
 
 cdir="$(dirname "$(readlink -f "${0}")")"
@@ -11,6 +12,6 @@ cdir="$(dirname "$(readlink -f "${0}")")"
 "${cdir}"/build.sh
 
 echo "sending firmware to ${1}"
-scp "${cdir}"/build/teensy_audio.hex "${1}":/tmp/
+scp "${cdir}"/build/teensy_audio.{hex,elf} "${1}":/tmp/
 scp "${cdir}"/../tools/flash_audio_board_firmware.sh "${1}":/tmp
 ssh "${1}" '/tmp/flash_audio_board_firmware.sh /tmp/teensy_audio.hex'
